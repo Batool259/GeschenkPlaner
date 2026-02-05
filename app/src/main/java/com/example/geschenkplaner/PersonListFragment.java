18b2fddf16bc9c0980eb12d12dbf0b447d1b96d5
Batool259 <batool.sawalha2509@gmail.com>
2026-02-05 16:16:38 +0100

    fragment_person_list.xml renamed

--- app/src/main/java/com/example/geschenkplaner/PersonListFragment.java
+++ app/src/main/java/com/example/geschenkplaner/PersonListFragment.java
@@ -47,7 +47,7 @@ public class PersonListFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_person_list, container, false);
+        return inflater.inflate(R.layout.fragment_add_persons, container, false);
     }
 
     @Override
