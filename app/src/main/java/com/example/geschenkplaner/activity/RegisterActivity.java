4da5b02ac20b29f6c9ccc5521517120556e3032d
Acee-33 <acelya.calin.2005@gmail.com>
2026-02-09 21:19:27 +0100

    Register: Aktivität angepasst

--- app/src/main/java/com/example/geschenkplaner/activity/RegisterActivity.java
+++ app/src/main/java/com/example/geschenkplaner/activity/RegisterActivity.java
@@ -1,4 +1,4 @@
-package com.example.geschenkplaner;
+package com.example.geschenkplaner.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +11,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.geschenkplaner.MainActivity;
+import com.example.geschenkplaner.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
